bdcdcab7cd636b5a4c93b0b09671a6c0b0fe5cf7
Leo M <leomaga.eq@gmail.com>
2024-12-18 12:43:58 -0300

    do ex01 and 02 my v

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, timesRepeated) {
+  if (timesRepeated < 0) return "ERROR";
+  let repeatedString = "";
+  for (i = 0; i < timesRepeated; i++) {
+    repeatedString += str;
+  }
+  return repeatedString;
 };
 
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
