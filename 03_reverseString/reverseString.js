fe588e34fb6ce3667071b63532df297c9e240285
Leo M <leomaga.eq@gmail.com>
2024-12-18 13:48:26 -0300

    complete ex03 my v

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let stringToRevert = str;
+  let revertedString = "";
+  for (let i = stringToRevert.length - 1; i >= 0; --i) {
+    revertedString += stringToRevert[i];
+  }
+  return revertedString;
+  //   // start iterating from the last to first?
+  //   // print iteration for testing purposes
 };
 
 // Do not edit below this line
