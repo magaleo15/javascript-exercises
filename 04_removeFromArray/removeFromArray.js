041d8d3bea8e5249ac5b8f793ecfd198ad2543ad
Leo M <leomaga.eq@gmail.com>
2024-12-18 22:52:43 -0300

    resolve multiple values test case

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,20 @@
 const removeFromArray = function (array, ...valuesToBeRemoved) {
-  for (i = 0; i < valuesToBeRemoved.length; i++) {
-    let values = valuesToBeRemoved;
-    if (array.indexOf(values[i]) >= 0) {
-      array.splice(array.indexOf(values[i]), 1);
+  valuesToBeRemoved.forEach((value) => {
+    while (array.includes(value)) {
+      array.splice(array.indexOf(value), 1);
     }
-  }
+  });
   return array;
 };
+
+// const removeFromArray = function (array, ...valuesToBeRemoved) {
+//   for (i = 0; i < valuesToBeRemoved.length; i++) {
+//     let values = valuesToBeRemoved;
+//     while (array.includes(values[i])) {
+//       array.splice(array.indexOf(values[i]), 1);
+//     }
+//   }
+//   return array;
+// };
 
 module.exports = removeFromArray;
