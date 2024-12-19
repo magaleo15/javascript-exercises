37487c599ee62b09a8bca2634c8c788213fb9060
Leo M <leomaga.eq@gmail.com>
2024-12-19 00:22:14 -0300

    resolve ex06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToFahrenheit = function (celciusTemperature) {
+  let fahrenheitTemperature = (9 * celciusTemperature) / 5 + 32;
+  return round(fahrenheitTemperature, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function (fahrenheitTemperature) {
+  let celciusTemperature = (fahrenheitTemperature - 32) * (5 / 9);
+  return round(celciusTemperature, 1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
